36b53eba3d8dcf25ad5ca77221e9d2083175a4d2
Rifatur Rahman <35681257+Rifatur@users.noreply.github.com>
2023-08-28 06:41:11 +0600

    Added Entities

---
+++ Shikka.Core/Entities/common/AuditableBaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Shikka.Core.Entities.common
+{
+    public abstract class AuditableBaseEntity : BaseEntity
+    {
+        public string? CreatedBy { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public string? LastModifiedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+    }
+}
---
+++ Shikka.Core/Entities/common/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Shikka.Core.Entities.common
+{
+    public abstract class BaseEntity
+    {
+        public virtual Guid Id { get; set; }
+    }
+}
