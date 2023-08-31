07be71055ed083758ee5af104582f3693483ff60
Rifatur Rahman <35681257+Rifatur@users.noreply.github.com>
2023-09-01 01:11:25 +0600

    added new models

---
+++ Shikka.Core/Entities/ClassHour.cs
@@ -0,0 +1,6 @@
+﻿namespace Shikka.Core.Entities
+{
+    public class ClassHour
+    {
+    }
+}
---
+++ Shikka.Core/Entities/ClassStandard .cs
@@ -0,0 +1,13 @@
+﻿using Shikka.Core.Entities.common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shikka.Core.Entities
+{
+    public class ClassStandard : AuditableBaseEntity
+    {
+        [Key]
+        public virtual Guid StandardId { get; set; }
+        public string? StandardName { get; set; }
+        public string? StandardDetails { get; set; }
+    }
+}
