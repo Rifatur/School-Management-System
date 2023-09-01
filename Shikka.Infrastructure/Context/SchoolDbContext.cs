3f9f025526bb552bffa3c9b6512dcb8066bb95a0
Rifatur Rahman <35681257+Rifatur@users.noreply.github.com>
2023-09-01 22:25:47 +0600

    Dbcontext file Added

---
+++ Shikka.Infrastructure/Context/SchoolDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shikka.Infrastructure.Context
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
